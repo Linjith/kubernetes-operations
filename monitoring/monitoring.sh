6e54992c463f531cdb5305535f20b5008404d02e
Linjith K Kunnon <linjith.kunnon@tavant.com>
2019-04-07 07:20:41 +0530

    added monitoring script

---
+++ monitoring/monitoring.sh
@@ -0,0 +1,15 @@
+#Install helm
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
+
+#Initialize helm
+helm init
+
+#Setup permission for tiller
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}' 
+
+#install prometheus-operator
+helm install  --name monitoring --namespace monitoring  stable/prometheus-operator
